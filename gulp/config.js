38e54a4ee2aec1df983a14cc8aec8df33f941fc0
Jesus A. Ferrer-Estupinan <jesus.ferrer@availity.com>
2015-05-22 15:39:19 -0400

    Close #40, Added in mask for the date field allowing for no breaks (ie. 05272015)

--- gulp/config.js
+++ gulp/config.js
@@ -112,7 +112,8 @@ module.exports = {
       './bower_components/lodash-compat/lodash.js',
       './bower_components/moment/moment.js',
       './bower_components/select2/select2.js',
-      './bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js'
+      './bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js',
+      './node_modules/jquery.inputmask/dist/inputmask/jquery.inputmask.js'
     ],
     destMaps: './maps', // this is relative path to the destDist above,
     dest: 'build/guide/js',
