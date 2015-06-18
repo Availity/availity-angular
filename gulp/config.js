b47b6d418564c1f7980f2d41a386cd87af4f328d
Robert Ventrone <rjventrone@gmail.com>
2015-06-18 11:37:48 -0400

    added api-spaces

--- gulp/config.js
+++ gulp/config.js
@@ -53,6 +53,7 @@ module.exports = {
       './lib/core/api/api-organizations.js',
       './lib/core/api/api-codes.js',
       './lib/core/api/api-user-permissions.js',
+      './lib/core/api/api-spaces.js',
       './lib/core/authorizations/user-authorizations.js',
       './lib/core/session/session.js',
       './lib/core/idle/idle.js',
