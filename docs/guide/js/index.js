25bea3df9a090e91f37c457509cb9f0d9d617a3b
Bobby Bennett <Bobby.Bennett@availity.com>
2015-09-21 22:06:53 -0400

    allow docs to have code examples for all language types

--- docs/guide/js/index.js
+++ docs/guide/js/index.js
@@ -71,7 +71,7 @@
       '</div>' +
       '</div>';
 
-    if ($(this).next().is('.language-markup')) {
+    if ($(this).next().is('[class*="language-"]')) {
       $(this).append($(btn));
     }
   });
@@ -80,7 +80,7 @@
 
     e.preventDefault();
 
-    var target = $(this).parents('.guide-example').next('.language-markup');
+    var target = $(this).parents('.guide-example').next('[class*="language-"]');
 
     if(target.is(':visible')) {
       target.velocity("slideUp", { duration: 200 });
