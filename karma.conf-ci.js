aeee020d42ad59e5bc8121c532e0b99879043188
Rob McGuinness <rob.mcguinness@availity.com>
2015-10-16 23:57:33 -0400

    disable some browser for saucelabs

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -9,24 +9,23 @@ module.exports = function(config) {
   // Check out https://saucelabs.com/platforms for all browser/OS combos
   var customLaunchers = {
 
-    sl_ms_edge: {
-      base: "SauceLabs",
-      browserName: "microsoftedge",
-      platform: "Windows 10",
-    },
-
-    sl_ie_11: {
-      base: "SauceLabs",
-      browserName: "Internet Explorer",
-      platform: "Windows 8.1",
-      version: "11"
-    },
-    sl_ie_10: {
-      base: "SauceLabs",
-      browserName: "Internet Explorer",
-      platform: "Windows 8",
-      version: "10"
-    },
+    // sl_ms_edge: {
+    //   base: "SauceLabs",
+    //   browserName: "microsoftedge",
+    //   platform: "Windows 10",
+    // },
+    // sl_ie_11: {
+    //   base: "SauceLabs",
+    //   browserName: "Internet Explorer",
+    //   platform: "Windows 8.1",
+    //   version: "11"
+    // },
+    // sl_ie_10: {
+    //   base: "SauceLabs",
+    //   browserName: "Internet Explorer",
+    //   platform: "Windows 8",
+    //   version: "10"
+    // },
     sl_ie_9: {
       base: 'SauceLabs',
       browserName: 'internet explorer',
