0baf272e6a31a5c6f42145decfa5a5ed430ba951
Sean Gillespie <sean.gillespie@availity.com>
2016-01-11 14:30:32 -0500

    bump package version v1.6.0

--- dist/availity-angular.js
+++ dist/availity-angular.js
@@ -1,5 +1,5 @@
 /**
- * availity-angular v1.5.1 -- January-11
+ * availity-angular v1.6.0 -- January-11
  * Copyright 2016 Availity, LLC 
  */
 
@@ -11,7 +11,7 @@
   'use strict';
 
   var availity = root.availity || {};
-  availity.VERSION = 'v1.5.1';
+  availity.VERSION = 'v1.6.0';
   availity.MODULE = 'availity';
   availity.core = angular.module(availity.MODULE, ['ng']);
 
