8f465c5f83d7a8c7e84df099ea4704e5dc614d75
Rob McGuinness <rob.mcguinness@availity.com>
2016-03-02 16:35:36 -0500

    bump package version v1.11.0

--- dist/availity-angular-ui.js
+++ dist/availity-angular-ui.js
@@ -1,9 +1,9 @@
 /**
- * availity-angular v1.10.1 -- February-25
+ * availity-angular v1.11.0 -- March-02
  * Copyright 2016 Availity, LLC 
  */
 
-// Source: -v1/lib/ui/index.js
+// Source: /lib/ui/index.js
 
 
 (function(root) {
@@ -28,7 +28,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/templates/template.js
+// Source: /lib/ui/templates/template.js
 (function(root) {
 
   'use strict';
@@ -58,7 +58,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/modal/modal.js
+// Source: /lib/ui/modal/modal.js
 (function(root) {
 
   'use strict';
@@ -393,7 +393,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/validation/form.js
+// Source: /lib/ui/validation/form.js
 /**
  * 1. All fields should be pristine on first load
  * 2. If field is modified an invalid the field should be marked with an error
@@ -589,7 +589,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/validation/field.js
+// Source: /lib/ui/validation/field.js
 (function(root) {
 
   'use strict';
@@ -849,7 +849,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/popover/popover.js
+// Source: /lib/ui/popover/popover.js
 (function(root) {
 
   'use strict';
@@ -958,7 +958,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/validation/container.js
+// Source: /lib/ui/validation/container.js
 (function(root) {
 
   'use strict';
@@ -1003,7 +1003,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/validation/adapter-bootstrap.js
+// Source: /lib/ui/validation/adapter-bootstrap.js
 (function(root) {
   'use strict';
 
@@ -1101,7 +1101,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/validation/adapter.js
+// Source: /lib/ui/validation/adapter.js
 (function(root) {
 
   'use strict';
@@ -1151,7 +1151,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/dropdown/dropdown.js
+// Source: /lib/ui/dropdown/dropdown.js
 (function(root) {
 
   'use strict';
@@ -1641,7 +1641,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/datepicker/datepicker.js
+// Source: /lib/ui/datepicker/datepicker.js
 /**
  * Inspiration https://github.com/mgcrea/angular-strap/blob/v0.7.8/src/directives/datepicker.js
  */
@@ -1890,7 +1890,7 @@
   });
 })(window);
 
-// Source: -v1/lib/ui/idle/idle-notifier.js
+// Source: /lib/ui/idle/idle-notifier.js
 (function(root) {
 
   'use strict';
@@ -2036,7 +2036,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/mask/mask.js
+// Source: /lib/ui/mask/mask.js
 (function(root) {
 
   'use strict';
@@ -2076,7 +2076,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/permissions/has-permission.js
+// Source: /lib/ui/permissions/has-permission.js
 (function(root) {
 
   'use strict';
@@ -2125,7 +2125,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/analytics/analytics.js
+// Source: /lib/ui/analytics/analytics.js
 (function(root) {
   'use strict';
 
@@ -2206,7 +2206,7 @@
   });
 })(window);
 
-// Source: -v1/lib/ui/placeholder/placeholder.js
+// Source: /lib/ui/placeholder/placeholder.js
 (function(root) {
 
   'use strict';
@@ -2243,7 +2243,7 @@
   });
 })(window);
 
-// Source: -v1/lib/ui/breadcrumbs/breadcrumbs.js
+// Source: /lib/ui/breadcrumbs/breadcrumbs.js
 (function(root) {
 
   'use strict';
@@ -2310,7 +2310,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/filters/approximate.js
+// Source: /lib/ui/filters/approximate.js
 (function(root) {
   'use strict';
 
@@ -2337,7 +2337,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/badge/badge.js
+// Source: /lib/ui/badge/badge.js
 (function(root) {
   'use strict';
 
@@ -2379,7 +2379,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/labels/removable-label.js
+// Source: /lib/ui/labels/removable-label.js
 (function(root) {
   'use strict';
 
@@ -2410,7 +2410,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/animation/loader.js
+// Source: /lib/ui/animation/loader.js
 (function(root) {
 
   'use strict';
@@ -2488,7 +2488,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/block/block.js
+// Source: /lib/ui/block/block.js
 (function(root) {
 
   'use strict';
@@ -2572,7 +2572,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/block/block-directive.js
+// Source: /lib/ui/block/block-directive.js
 (function(root) {
 
   'use strict';
@@ -2599,7 +2599,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/tabs/tabs.js
+// Source: /lib/ui/tabs/tabs.js
 /*
 * Inspired by https://github.com/angular-ui/bootstrap/blob/master/src/tabs/tabs.js
 */
@@ -2774,7 +2774,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/scroll-pagination/scroll-pagination.js
+// Source: /lib/ui/scroll-pagination/scroll-pagination.js
 (function(root) {
 
   'use strict';
@@ -2994,7 +2994,7 @@
 
 })(window);
 
-// Source: -v1/lib/ui/dimmer/dimmer.js
+// Source: /lib/ui/dimmer/dimmer.js
 // Original => http://bootsnipp.com/snippets/78VV
 (function(root) {
 
