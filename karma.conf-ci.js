2dc735749736f608c35a7c621909c78e20afbab8
Rob McGuinness <rob.mcguinness@availity.com>
2016-07-21 23:42:57 -0400

    add caching to travis and expand karma timeouts

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -52,6 +52,11 @@ module.exports = function(config) {
     files: files,
     autoWatch: false,
     browsers: Object.keys(customLaunchers),
+    browserDisconnectTimeout: 10000,      // default 2000
+    browserDisconnectTolerance: 2,        // default 0
+    browserNoActivityTimeout: 4 * 60000,  // default 10000
+    captureTimeout: 4 * 60000,            // default 60000
+    concurrency: 5,
     customLaunchers: customLaunchers,
     frameworks: ['jasmine'],
     reporters: ['mocha', 'saucelabs'],
