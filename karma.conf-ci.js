5be2388a10430e96ac9f9f8c367a35ca80c0fddc
Rob McGuinness <rob.mcguinness@availity.com>
2016-07-21 23:28:45 -0400

    modify karma setting for sauce labs

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -19,24 +19,6 @@ module.exports = function(config) {
   // Check out https://saucelabs.com/platforms for all browser/OS combos
   var customLaunchers = {
 
-    // sl_ms_edge: {
-    //   base: "SauceLabs",
-    //   browserName: "microsoftedge",
-    //   platform: "Windows 10",
-    // },
-    // sl_ie_11: {
-    //   base: "SauceLabs",
-    //   browserName: "Internet Explorer",
-    //   platform: "Windows 8.1",
-    //   version: "11"
-    // },
-    // sl_ie_10: {
-    //   base: "SauceLabs",
-    //   browserName: "Internet Explorer",
-    //   platform: "Windows 8",
-    //   version: "10"
-    // },
-
     sl_ie_9: {
       base: 'SauceLabs',
       browserName: 'internet explorer',
@@ -58,7 +40,9 @@ module.exports = function(config) {
   };
 
   if(process.env.TRAVIS_JOB_NUMBER) {
+    sauceLabs.build = "TRAVIS #" + process.env.TRAVIS_BUILD_NUMBER + " (" + process.env.TRAVIS_BUILD_ID + ")";
     sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+    sauceLabs.tags = [process.env.TRAVIS_BRANCH, process.env.TRAVIS_PULL_REQUEST];
   } else {
     sauceLabs.startConnect = true;
   }
@@ -75,10 +59,6 @@ module.exports = function(config) {
     colors: true,
     sauceLabs: sauceLabs,
     logLevel: config.LOG_INFO,
-    captureTimeout: 120000,
-    browserDisconnectTimeout: 10000,
-    browserDisconnectTolerance: 2,
-    browserNoActivityTimeout: 20000,
     singleRun: true,
     preprocessors: {
       'lib/**/*-tpl.html': ['ng-html2js'],
