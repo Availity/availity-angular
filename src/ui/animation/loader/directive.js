3ec54fc421bad9cb1332e12c523a121a0d5eba67
Rob McGuinness <rob.mcguinness@availity.com>
2016-07-25 23:46:14 -0400

    refactor avLoader with controller syntax

--- src/ui/animation/loader/directive.js
+++ src/ui/animation/loader/directive.js
@@ -1,49 +1,66 @@
+import 'velocity-animate';
+
 import templateUrl from './template.html';
+import Base from '../../base';
 import ngModule from '../../module';
 
-ngModule.directive('avLoader', function() {
+class AvLoaderController extends Base {
+
+  static $inject = ['$element'];
+
+  constructor(...args) {
+
+    super(...args);
+
+    this.active = false;
+  }
+
+  start() {
+    this.active = true;
+    this.animate();
+  }
+
+  animate() {
+
+    const self = this;
+
+    this.av.$element
+    .find('.loading-bullet')
+    .velocity('transition.slideRightIn', { stagger: 250 })
+    .velocity({ opacity: 0 }, {
+      delay: 750,
+      duration: 500,
+      complete() {
+        if (self.active) {
+          setTimeout( () => { self.animate() }, 500);
+        } else {
+          self.endAnimation();
+        }
+      }
+    });
+
+  }
+
+  endAnimation = function() {
+    this.av.$element.find('.loading-bullet').velocity('stop', true);
+    this.av.$element.removeData();
+  }
+
+  $destroy() {
+    this.active = false;
+  }
+
+  $postLink() {
+    this.start();
+  }
+}
+
+ngModule.directive('avLoader', () => {
   return {
     restrict: 'AE',
     replace: true,
-    templateUrl,
-    link(scope, element, attr) {
-
-      let active;
-
-
-      const endAnimation = () => {
-        element.find('.loading-bullet').velocity('stop', true);
-        element.removeData();
-      };
-
-      const animate = () => {
-        element
-        .find('.loading-bullet')
-        .velocity('transition.slideRightIn', { stagger: 250 })
-        .velocity({ opacity: 0 }, {
-          delay: 750,
-          duration: 500,
-          complete() {
-            if (active) {
-              setTimeout(function() {animate() }, 500);
-            } else {
-              endAnimation();
-            }
-          }
-        });
-      };
-
-
-      if (!attr.delay) {
-        active = true;
-        animate();
-      }
-
-      scope.$on('$destroy', function() {
-        active = false;
-      });
-
-    }
+    controller: AvLoaderController,
+    templateUrl
   };
 });
 
