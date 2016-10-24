80aeb873ffd78dbfc63ce33ade19677c8bc1f738
Rob McGuinness <rob.mcguinness@availity.com>
2016-10-24 14:31:08 -0400

    add back placeholder spec

--- src/ui/placeholder/tests/placeholder-spec-2.js
+++ src/ui/placeholder/tests/placeholder-spec-2.js
@@ -1,17 +1,18 @@
-/* global describe, angular, it, jasmine, availity, expect, beforeEach, module, inject */
-
-/* global describe, angular, it, jasmine, availity, expect, beforeEach, module, inject */
+/* global describe, it, jasmine, expect, beforeEach, module, inject */
 
 import $ from 'jquery';
+import angular from 'angular';
+import Tester from 'tester';
+import '../';
 
-describe('placeholder', function() {
+describe('placeholder', () => {
 
   let $el;
   const placeholderSniffer = null;
   let $compile;
   let scope;
+  const tester = new Tester();
 
-  // jscs: disable
   const fixtures = {
     'default': '<input type="text" id="test" placeholder="testing">',
     'model': '<input type="text" name="model" data-ng-model="demo.data" placeholder="testing">',
@@ -20,26 +21,27 @@ describe('placeholder', function() {
     'datepicker': '<input data-ng-model="demo.data" name="date" type="text" data-av-mask="date" data-av-datepicker placeholder="testing">',
     'custom': '<input data-ng-model="demo.data" name="custom" type="text" data-av-mask="9a99a" placeholder="testing">'
   };
-  // jscs: enable
 
-  beforeEach(function() {
+  beforeEach(() => {
+
     module('ng.shims.placeholder');
-    module('availity', function(avValProvider) {
+    module('availity', avValProvider => {
+
       avValProvider.addRules({
-        'default': {
-          'lastName': {
-            'size': {
-              'min': 2,
-              'max': 10,
-              'message': 'Last name must be between 2 and 10 characters.'
+        regular: {
+          lastName: {
+            size: {
+              min: 2,
+              max: 10,
+              message: 'Last name must be between 2 and 10 characters.'
             },
-            'required': {
-              'message': 'Last name is required.'
+            required: {
+              message: 'Last name is required.'
             }
           },
-          'dateFormat': {
-            'format': 'MM/DD/YYYY',
-            'message': 'Format needs to be MM/DD/YYYY'
+          dateFormat: {
+            format: 'MM/DD/YYYY',
+            message: 'Format needs to be MM/DD/YYYY'
           }
         }
       });
@@ -47,15 +49,15 @@ describe('placeholder', function() {
     module('availity.ui');
   });
 
-  availity.mock.directiveSpecHelper();
+  tester.directiveSpecHelper();
 
   beforeEach(inject(function($injector, _placeholderSniffer_) {
     $compile = $injector.get('$compile');
     scope = $injector.get('$rootScope');
     scope.form = {};
-    availity.mock.$scope.demo = {};
+    tester.$scope.demo = {};
     placeholderSniffer = _placeholderSniffer_;
-    placeholderSniffer.hasPlaceholder = function() {
+    placeholderSniffer.hasPlaceholder = () => {
       return false; // force every browser to run the shim
     };
   }));
@@ -63,7 +65,7 @@ describe('placeholder', function() {
   it('should not dirty the value', function() {
 
     // why does this pass on travis but fails locally?
-    // $el = availity.mock.compileDirective(fixtures.default);
+    // $el = tester.compileDirective(fixtures.default);
     // expect($el.val()).toEqual('testing');
     // expect($el.hasClass('empty')).toBe(true);
 
@@ -74,7 +76,7 @@ describe('placeholder', function() {
   });
 
   it('should not dirty the focused value', function() {
-    $el = availity.mock.compileDirective(fixtures.default);
+    $el = tester.compileDirective(fixtures.default);
     $el.triggerHandler('focus');
     expect($el.val()).toEqual('');
     expect($el.hasClass('empty')).toBe(false);
@@ -82,16 +84,16 @@ describe('placeholder', function() {
   });
 
   it('should not dirty the model', function() {
-    availity.mock.$scope.demo = {
+    tester.$scope.demo = {
       data: ''
     };
-    $el = availity.mock.compileDirective(fixtures.model);
-    availity.mock.flush();
-    expect(availity.mock.$scope.demo.data).toEqual('');
+    $el = tester.compileDirective(fixtures.model);
+    tester.flush();
+    expect(tester.$scope.demo.data).toEqual('');
   });
 
   it('should get value', function() {
-    $el = availity.mock.compileDirective(fixtures.testValue);
+    $el = tester.compileDirective(fixtures.testValue);
     expect($el.val()).toEqual('test');
 
   });
@@ -99,31 +101,31 @@ describe('placeholder', function() {
   describe('with validation', function() {
 
     beforeEach(function() {
-      availity.mock.$scope.demo = {};
-      availity.mock.$scope.demo.rules = 'default';
+      tester.$scope.demo = {};
+      tester.$scope.demo.rules = 'default';
 
       const template = `
         <form name="myForm" data-av-val-form="default">
           <input data-ng-model="demo.lastName" name="lastName" type="text" data-av-val-field="lastName" placeholder="test"/>
         </form>`;
 
-      $el = availity.mock.compileDirective(template);
+      $el = tester.compileDirective(template);
     });
 
     it('should be valid with model updated', function() {
-      availity.mock.$scope.myForm.lastName.$setViewValue('lastName');
-      availity.mock.$scope.$digest();
+      tester.$scope.myForm.lastName.$setViewValue('lastName');
+      tester.$scope.$digest();
 
-      expect(availity.mock.$scope.myForm.lastName.$invalid).toBe(false);
-      expect(availity.mock.$scope.demo.lastName).toEqual('lastName');
+      expect(tester.$scope.myForm.lastName.$invalid).toBe(false);
+      expect(tester.$scope.demo.lastName).toEqual('lastName');
     });
 
     it('should NOT be valid and model should NOT be updated', function() {
-      availity.mock.$scope.myForm.lastName.$setViewValue('1');
-      availity.mock.$scope.$digest();
+      tester.$scope.myForm.lastName.$setViewValue('1');
+      tester.$scope.$digest();
 
-      expect(availity.mock.$scope.myForm.lastName.$invalid).toBe(true);
-      expect(availity.mock.$scope.demo.lastName).toBeUndefined();
+      expect(tester.$scope.myForm.lastName.$invalid).toBe(true);
+      expect(tester.$scope.demo.lastName).toBeUndefined();
     });
   });
 
@@ -131,8 +133,8 @@ describe('placeholder', function() {
 
     beforeEach(function() {
 
-      availity.mock.$scope.demo = {};
-      availity.mock.$scope.submit = jasmine.createSpy('submit');
+      tester.$scope.demo = {};
+      tester.$scope.submit = jasmine.createSpy('submit');
 
       const template = `
         <form name="myForm" data-ng-submit="submit()" id="myForm">
@@ -141,22 +143,22 @@ describe('placeholder', function() {
           <input type="submit" id="submit" value="submit"/>
         </form>`;
 
-      $el = availity.mock.compileDirective(template);
+      $el = tester.compileDirective(template);
     });
 
     it('should not dirty empty fields', function() {
 
       $el.triggerHandler('submit');
-      availity.mock.$scope.$digest();
+      tester.$scope.$digest();
 
       expect($('#input1').val()).toEqual('test');
       expect($('#input2').val()).toEqual('test');
       expect($('#input1').hasClass('empty')).toBe(true);
       expect($('#input2').hasClass('empty')).toBe(true);
 
-      expect(availity.mock.$scope.demo.firstName).toBeUndefined();
-      expect(availity.mock.$scope.demo.lastName).toBeUndefined();
-      expect(availity.mock.$scope.submit).toHaveBeenCalled();
+      expect(tester.$scope.demo.firstName).toBeUndefined();
+      expect(tester.$scope.demo.lastName).toBeUndefined();
+      expect(tester.$scope.submit).toHaveBeenCalled();
 
     });
 
@@ -190,23 +192,23 @@ describe('placeholder', function() {
 
     it('should work with phone mask', function() {
 
-      availity.mock.$scope.demo.data = null;
-      $el = availity.mock.compileDirective(fixtures.phone);
+      tester.$scope.demo.data = null;
+      $el = tester.compileDirective(fixtures.phone);
       $el.val('9041234567').trigger('change');
-      availity.mock.$scope.$digest();
+      tester.$scope.$digest();
 
-      expect(availity.mock.$scope.demo.data).toBe('(904) 123-4567');
+      expect(tester.$scope.demo.data).toBe('(904) 123-4567');
     });
 
     it('should work with avDatepicker', function() {
 
-      availity.mock.$scope.demo.data = null;
-      $el = availity.mock.compileDirective(fixtures.datepicker);
+      tester.$scope.demo.data = null;
+      $el = tester.compileDirective(fixtures.datepicker);
 
       $el.val('12122015').trigger('change');
-      availity.mock.$scope.$digest();
+      tester.$scope.$digest();
 
-      const date = availity.mock.$scope.demo.data;
+      const date = tester.$scope.demo.data;
 
       expect(date instanceof Date).toBe(true);
       expect(date.getFullYear()).toBe(2015);
@@ -216,13 +218,13 @@ describe('placeholder', function() {
 
     it('should work with custom mask format', function() {
 
-      availity.mock.$scope.demo.data = null;
-      $el = availity.mock.compileDirective(fixtures.custom);
+      tester.$scope.demo.data = null;
+      $el = tester.compileDirective(fixtures.custom);
 
       $el.val('1a234').trigger('change');
-      availity.mock.$scope.$digest();
+      tester.$scope.$digest();
 
-      expect(availity.mock.$scope.demo.data).toBe('1a23_');
+      expect(tester.$scope.demo.data).toBe('1a23_');
     });
   });
 
