c8d799ea925569163d882ce47aca2ac10562dfe1
Christie Baker <Christie.Baker@availity.com>
2018-01-30 14:11:43 -0600

    fix upload tests

--- src/ui/uploads/tests/upload-progress-bar-spec.js
+++ src/ui/uploads/tests/upload-progress-bar-spec.js
@@ -17,17 +17,21 @@ describe('uploadProgessBar', () => {
   it('should render with defaults', () => {
     tester.$scope.upload = {
       percentage: 0,
-      onProgress: []
+      onProgress: [],
+      onSuccess: [],
+      onError: []
     };
     const el = tester.compileDirective(template, null, null);
     const progressBar = el.find('.progress');
     expect(progressBar.hasClass('progress')).toBeTruthy();
   });
 
-  it('should render with 50% progess', () => {
+  it('should render with 50% progress', () => {
     tester.$scope.upload = {
       percentage: 50,
       onProgress: [],
+      onSuccess: [],
+      onError: [],
       completed: false
     };
     const el = tester.compileDirective(template, null, null);
@@ -37,14 +41,17 @@ describe('uploadProgessBar', () => {
     expect(progressBar.attr('style')).toBe('width: 50%;');
   });
 
-  it('should render with with 100% progress', () => {
+  it('should render with 100% progress', () => {
     tester.$scope.upload = {
       percentage: 100,
       onProgress: [],
-      completed: false
+      onSuccess: [],
+      onError: [],
+      completed: true
     };
     const el = tester.compileDirective(template, null, null);
     tester.$scope.upload.onProgress.forEach(cb => cb(50, 100));
+    tester.$scope.upload.onSuccess.forEach(cb => cb(50, 100));
     tester.$scope.$apply();
     const progress = el.find('.progress');
     const progressBar = el.find('.progress-bar');
